35b93c0c14ff0b5ed8dcecb84ab9392594683690
zzq <475731070@qq.com>
2022-12-03 10:53:32 +0800

    tmp

--- V1/src/modules/agents/rnn_agent.py
+++ V1/src/modules/agents/rnn_agent.py
@@ -1,5 +1,7 @@
+import torch as th
 import torch.nn as nn
 import torch.nn.functional as F
+from icecream import ic 
 
 
 class RNNAgent(nn.Module):
@@ -18,7 +20,10 @@ class RNNAgent(nn.Module):
         # make hidden states on same device as model
         return self.fc1.weight.new(1, self.args.hidden_dim).zero_()
 
-    def forward(self, inputs, hidden_state):
+    def forward(self, inputs, hidden_state, proxy_z=None):
+        if proxy_z is not None:
+            assert inputs.shape[:-1] == proxy_z.shape[:-1], ic(inputs.shape, proxy_z.shape)
+            inputs = th.cat((inputs, proxy_z), dim=-1)
         x = F.relu(self.fc1(inputs))
         h_in = hidden_state.reshape(-1, self.args.hidden_dim)
         if self.args.use_rnn:
--- V1/src/modules/agents/rnn_ns_agent.py
+++ V1/src/modules/agents/rnn_ns_agent.py
@@ -15,23 +15,40 @@ class RNNNSAgent(nn.Module):
         # make hidden states on same device as model
         return th.cat([a.init_hidden() for a in self.agents])
 
-    def forward(self, inputs, hidden_state):
+    def forward(self, inputs, hidden_state, proxy_z=None):
         hiddens = []
         qs = []
-        if inputs.size(0) == self.n_control:
-            for i in range(self.n_control):
-                q, h = self.agents[i](inputs[i].unsqueeze(0), hidden_state[:, i])
-                hiddens.append(h)
-                qs.append(q)
-            return th.cat(qs), th.cat(hiddens).unsqueeze(0)
+        if proxy_z is not None:
+            assert inputs.shape[:-1] == proxy_z.shape[:-1], print(inputs.shape, proxy_z.shape)
+            if inputs.size(0) == self.n_control:
+                for i in range(self.n_control):
+                    q, h = self.agents[i](inputs[i].unsqueeze(0), hidden_state[:, i], proxy_z[i].unsqueeze(0))
+                    hiddens.append(h)
+                    qs.append(q)
+                return th.cat(qs), th.cat(hiddens).unsqueeze(0)
+            else:
+                for i in range(self.n_control):
+                    inputs = inputs.view(-1, self.n_control, self.input_shape)
+                    proxy_z = proxy_z.view(-1, self.n_control, self.args.proxy_z_dim)
+                    q, h = self.agents[i](inputs[:, i], hidden_state[:, i], proxy_z[:, i])
+                    hiddens.append(h.unsqueeze(1))
+                    qs.append(q.unsqueeze(1))
+                return th.cat(qs, dim=-1).view(-1, q.size(-1)), th.cat(hiddens, dim=1)
         else:
-            for i in range(self.n_control):
-                inputs = inputs.view(-1, self.n_control, self.input_shape)
-                q, h = self.agents[i](inputs[:, i], hidden_state[:, i])
-                hiddens.append(h.unsqueeze(1))
-                qs.append(q.unsqueeze(1))
-            return th.cat(qs, dim=-1).view(-1, q.size(-1)), th.cat(hiddens, dim=1)
-
+            if inputs.size(0) == self.n_control:
+                    for i in range(self.n_control):
+                        q, h = self.agents[i](inputs[i].unsqueeze(0), hidden_state[:, i])
+                        hiddens.append(h)
+                        qs.append(q)
+                    return th.cat(qs), th.cat(hiddens).unsqueeze(0)
+            else:
+                for i in range(self.n_control):
+                    inputs = inputs.view(-1, self.n_control, self.input_shape)
+                    q, h = self.agents[i](inputs[:, i], hidden_state[:, i])
+                    hiddens.append(h.unsqueeze(1))
+                    qs.append(q.unsqueeze(1))
+                return th.cat(qs, dim=-1).view(-1, q.size(-1)), th.cat(hiddens, dim=1)
+                    
     def cuda(self, device=None):
         if not device:
             device = self.args.device
